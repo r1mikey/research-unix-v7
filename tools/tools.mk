8de25708e4156e73625a8f4e261e77c5377e1bfa
Michael van der Westhuizen <r1mikey@gmail.com>
2020-05-22 20:54:35 +0100

    Remove support for the Carlson Minot compiler
    The Linaro gcc embedded toolchain now supports armv6k.  Simplify the
    build and setup by removing support for the outdated Carlson Minot
    compiler, leaving only the Linaro toolchain supported.
    
    Signed-off-by: Michael van der Westhuizen <r1mikey@gmail.com>

--- tools/tools.mk
+++ tools/tools.mk
@@ -1,32 +1,17 @@
 CURRENT_OS := $(strip $(shell uname -s))
-HAS_CARLSON_MINOT_USR_LOCAL := $(shell test -d /usr/local/carlson-minot/crosscompilers/bin && echo "yes" || echo "no")
-HAS_CARLSON_MINOT_IN_HOME := $(shell test -d $(HOME)/Downloads/usr/local/carlson-minot/crosscompilers/bin && echo "yes" || echo "no")
 
 ifeq ($(CURRENT_OS),Darwin)
-ifeq ($(HAS_CARLSON_MINOT_USR_LOCAL),yes)
-COMPILERS_DIR ?= /usr/local/carlson-minot/crosscompilers/bin
-else
-ifeq ($(HAS_CARLSON_MINOT_IN_HOME),yes)
-COMPILERS_DIR ?= $(HOME)/Downloads/usr/local/carlson-minot/crosscompilers/bin
-else
-$(error Could not find a Carlson Minot cross compiler in either /usr/local/carlson-minot/crosscompilers/bin or $(HOME)/Downloads/usr/local/carlson-minot/crosscompilers/bin)
-endif
-endif
-COMPILERS_PREFIX ?= arm-none-eabi-
+CROSS_COMPILE ?= arm-none-eabi-
 else ifeq ($(CURRENT_OS),Linux)
-ifeq ($(COMPILERS_DIR),)
 LINARO_DIR := $(shell (cd /opt && ls -1d gcc-linaro-*arm-eabi | tail -1))
 ifeq ($(LINARO_DIR),)
-$(error Could not find a Linaro gcc cross compiler for arm-eabi in /opt)
+CROSS_COMPILE ?= arm-eabi-
+else
+CROSS_COMPILE ?= /opt/$(LINARO_DIR)/bin/arm-eabi-
 endif
-COMPILERS_DIR ?= /opt/$(LINARO_DIR)/bin
-endif
-COMPILERS_PREFIX ?= arm-eabi-
 else
 $(error $(CURRENT_OS) is not a supported OS)
 endif
-
-CROSS_COMPILE ?= $(COMPILERS_DIR)/$(COMPILERS_PREFIX)
 
 CC = $(CROSS_COMPILE)gcc
 CCLD = $(CROSS_COMPILE)gcc
@@ -42,21 +27,22 @@ NM = $(CROSS_COMPILE)nm
 MKAOUT ?= $(SUBDIR_LEVEL)/tools/mkaout.py
 YACC = $(SUBDIR_LEVEL)/tools/yacc/yacc
 
+FLOAT_ABI_FLAG ?= -mfloat-abi=hard
 CPPFLAGS ?= -I$(SUBDIR_LEVEL)/usr/include
 # -fstack-usage option and -fcallgraph-info
 # -Wall -pedantic
-CFLAGS ?= -std=c89 -ffreestanding -nostdlib -nostdinc -nostartfiles -marm -march=armv6k -mabi=aapcs -mfloat-abi=hard -ggdb
+CFLAGS ?= -std=c89 -ffreestanding -nostdlib -nostdinc -nostartfiles -marm -march=armv6k+vfpv2 -mabi=aapcs $(FLOAT_ABI_FLAG) -ggdb
 ASFLAGS ?= -ggdb -march=armv6k
 LDFLAGS ?= $(CPPFLAGS) $(CFLAGS) -Wl,--build-id=none -T$(SUBDIR_LEVEL)/tools/cinit.lds
 LOADLIBES ?= -L$(SUBDIR_LEVEL)/usr/src/libc
 LDLIBS ?= -lc
 
 CPP11SPECIALFLAGS =
-C11SPECIALFLAGS = -std=c11 -ffreestanding -nostdlib -nostartfiles -marm -march=armv6k -mabi=aapcs -mfloat-abi=hard -ggdb
+C11SPECIALFLAGS = -std=c11 -ffreestanding -nostdlib -nostartfiles -marm -march=armv6k+vfpv2 -mabi=aapcs $(FLOAT_ABI_FLAG) -ggdb
 
 KERNEL_LOADLIBES =
 KERNEL_CPPFLAGS =
 KERNEL_LDLIBS =
-KERNEL_CFLAGS = -std=c89 -ffreestanding -nostdlib -nostartfiles -marm -march=armv6k -mabi=aapcs -ggdb
-KERNEL_C11SPECIALFLAGS = -std=c11 -ffreestanding -nostdlib -nostartfiles -marm -march=armv6k -mabi=aapcs -ggdb
-KERNEL_LDFLAGS = $(KERNEL_CPPFLAGS) $(KERNEL_CFLAGS) -Wl,--build-id=none -Wl,--defsym,PROGRAM_ENTRY_OFFSET=$(PROGRAM_ENTRY_OFFSET) -Wl,--defsym,KERNVIRTADDR=$(KERNVIRTADDR)
+KERNEL_CFLAGS = -std=c89 -ffreestanding -nostdlib -nostartfiles -marm -march=armv6k+vfpv2 -mabi=aapcs -ggdb
+KERNEL_C11SPECIALFLAGS = -std=c11 -ffreestanding -nostdlib -nostartfiles -marm -march=armv6k+vfpv2 -mabi=aapcs -ggdb
+KERNEL_LDFLAGS = $(KERNEL_CPPFLAGS) $(KERNEL_CFLAGS) -Wl,--build-id=none -Wl,--defsym,PROGRAM_ENTRY_OFFSET=$(PROGRAM_ENTRY_OFFSET) -Wl,--defsym,KERNVIRTADDR=$(KERNVIRTADDR) -Wl,-N
