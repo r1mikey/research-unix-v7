d9916182e539f7353ca4434fb03bf463b03cd019
Michael van der Westhuizen <r1mikey@gmail.com>
2020-06-19 22:17:21 +0100

    kernel: flesh out types

--- usr/sys/h/param.h
+++ usr/sys/h/param.h
@@ -141,12 +141,14 @@ $
 unsigned char and unsigned long DO NOT EXIST
 #endif
 
-typedef signed char	i8;
-typedef unsigned char	u8;
-typedef signed short	i16;
-typedef unsigned short	u16;
-typedef signed int	i32;
-typedef unsigned int	u32;
+typedef signed char	        i8;
+typedef unsigned char	        u8;
+typedef signed short	        i16;
+typedef unsigned short	        u16;
+typedef signed int	        i32;
+typedef unsigned int	        u32;
+typedef signed long long	i64;
+typedef unsigned long long	u64;
 
 typedef	struct { i16 r[1]; } *	physadr;
 typedef	i32		daddr_t;
--- usr/sys/h/types.h
+++ usr/sys/h/types.h
@@ -1,9 +1,11 @@
-typedef signed char     i8;
-typedef unsigned char   u8;
-typedef signed short    i16;
-typedef unsigned short  u16;
-typedef signed int      i32;
-typedef unsigned int    u32;
+typedef signed char		i8;
+typedef unsigned char		u8;
+typedef signed short		i16;
+typedef unsigned short		u16;
+typedef signed int		i32;
+typedef unsigned int		u32;
+typedef signed long long	i64;
+typedef unsigned long long	u64;
 
 typedef i32             daddr_t;
 typedef i8 *            caddr_t;
