876f988487e82c7627ec1a4c81038ff6d3220d6b
Michael van der Westhuizen <r1mikey@gmail.com>
2020-12-13 14:36:17 +0000

    Properly declare switch struct arrays
    Signed-off-by: Michael van der Westhuizen <r1mikey@gmail.com>

--- usr/sys/bcm283x/conf/bcm283x_conf.c
+++ usr/sys/bcm283x/conf/bcm283x_conf.c
@@ -18,17 +18,17 @@ extern struct	buf	sdxtab;
 
 struct	bdevsw	bdevsw[] =
 {
-	nodev, nodev, nodev, 0,	/* rk = 0 */
-	nodev, nodev, nodev, 0, /* rp = 1 */
-	nodev, nodev, nodev, 0, /* rf = 2 */
-	nodev, nodev, nodev, 0, /* tm = 3 */
-	nodev, nodev, nodev, 0, /* tc = 4 */
-	nodev, nodev, nodev, 0, /* hs = 5 */
-	nodev, nodev, nodev, 0, /* hp = 6 */
-	nodev, nodev, nodev, 0,	/* ht = 7 */
-	nodev, nodev, nodev, 0, /* rl = 8 */
-	nulldev, nulldev, sdxstrategy, &sdxtab, /* sdx = 9 */
-	0
+	{nodev, nodev, nodev, 0},	/* rk = 0 */
+	{nodev, nodev, nodev, 0}, /* rp = 1 */
+	{nodev, nodev, nodev, 0}, /* rf = 2 */
+	{nodev, nodev, nodev, 0}, /* tm = 3 */
+	{nodev, nodev, nodev, 0}, /* tc = 4 */
+	{nodev, nodev, nodev, 0}, /* hs = 5 */
+	{nodev, nodev, nodev, 0}, /* hp = 6 */
+	{nodev, nodev, nodev, 0},	/* ht = 7 */
+	{nodev, nodev, nodev, 0}, /* rl = 8 */
+	{nulldev, nulldev, sdxstrategy, &sdxtab}, /* sdx = 9 */
+	{0}
 };
 
 int	sdxread(), sdxwrite();
@@ -44,35 +44,35 @@ int bcm283x_pl011stop();
 
 struct	cdevsw	cdevsw[] =
 {
-	bcm283x_pl011open, bcm283x_pl011close, bcm283x_pl011read, bcm283x_pl011write, bcm283x_pl011ioctl, bcm283x_pl011stop, 0,	/* console = 0 */
-	nodev, nodev, nodev, nodev, nodev, nulldev, 0, /* pc = 1 */
-	nodev, nodev, nodev, nodev, nodev, nulldev, 0, /* lp = 2 */
-	nodev, nodev, nodev, nodev, nodev, nulldev, 0, /* dc = 3 */
-	nodev, nodev, nodev, nodev, nodev, nulldev, 0, /* dh = 4 */
-	nodev, nodev, nodev, nodev, nodev, nulldev, 0, /* dp = 5 */
-	nodev, nodev, nodev, nodev, nodev, nulldev, 0, /* dj = 6 */
-	nodev, nodev, nodev, nodev, nodev, nulldev, 0, /* dn = 7 */
-	nulldev, nulldev, mmread, mmwrite, nodev, nulldev, 0, 	/* mem = 8 */
-	nodev, nodev, nodev, nodev, nodev, nulldev, 0,	/* rk = 9 */
-	nodev, nodev, nodev, nodev, nodev, nulldev, 0, /* rf = 10 */
-	nodev, nodev, nodev, nodev, nodev, nulldev, 0, /* rp = 11 */
-	nodev, nodev, nodev, nodev, nodev, nulldev, 0, /* tm = 12 */
-	nodev, nodev, nodev, nodev, nodev, nulldev, 0, /* hs = 13 */
-	nodev, nodev, nodev, nodev, nodev, nulldev, 0, /* hp = 14 */
-	nodev, nodev, nodev, nodev, nodev, nulldev, 0,	/* ht = 15 */
-	nodev, nodev, nodev, nodev, nodev, nulldev, 0, /* du = 16 */
-	syopen, nulldev, syread, sywrite, sysioctl, nulldev, 0, /* tty = 17 */
-	nodev, nodev, nodev, nodev, nodev, nulldev, 0, /* rl = 18 */
-	nulldev, nulldev, sdxread, sdxwrite, nodev, nulldev, 0, /* sdx = 19 */
-	0
+	{bcm283x_pl011open, bcm283x_pl011close, bcm283x_pl011read, bcm283x_pl011write, bcm283x_pl011ioctl, bcm283x_pl011stop, 0},	/* console = 0 */
+	{nodev, nodev, nodev, nodev, nodev, nulldev, 0}, /* pc = 1 */
+	{nodev, nodev, nodev, nodev, nodev, nulldev, 0}, /* lp = 2 */
+	{nodev, nodev, nodev, nodev, nodev, nulldev, 0}, /* dc = 3 */
+	{nodev, nodev, nodev, nodev, nodev, nulldev, 0}, /* dh = 4 */
+	{nodev, nodev, nodev, nodev, nodev, nulldev, 0}, /* dp = 5 */
+	{nodev, nodev, nodev, nodev, nodev, nulldev, 0}, /* dj = 6 */
+	{nodev, nodev, nodev, nodev, nodev, nulldev, 0}, /* dn = 7 */
+	{nulldev, nulldev, mmread, mmwrite, nodev, nulldev, 0}, 	/* mem = 8 */
+	{nodev, nodev, nodev, nodev, nodev, nulldev, 0},	/* rk = 9 */
+	{nodev, nodev, nodev, nodev, nodev, nulldev, 0}, /* rf = 10 */
+	{nodev, nodev, nodev, nodev, nodev, nulldev, 0}, /* rp = 11 */
+	{nodev, nodev, nodev, nodev, nodev, nulldev, 0}, /* tm = 12 */
+	{nodev, nodev, nodev, nodev, nodev, nulldev, 0}, /* hs = 13 */
+	{nodev, nodev, nodev, nodev, nodev, nulldev, 0}, /* hp = 14 */
+	{nodev, nodev, nodev, nodev, nodev, nulldev, 0},	/* ht = 15 */
+	{nodev, nodev, nodev, nodev, nodev, nulldev, 0}, /* du = 16 */
+	{syopen, nulldev, syread, sywrite, sysioctl, nulldev, 0}, /* tty = 17 */
+	{nodev, nodev, nodev, nodev, nodev, nulldev, 0}, /* rl = 18 */
+	{nulldev, nulldev, sdxread, sdxwrite, nodev, nulldev, 0}, /* sdx = 19 */
+	{0}
 };
 
 extern caddr_t ttwrite(struct tty *tp);
 int	ttyopen(), ttyclose(), ttread(), ttyinput(), ttstart();
 struct	linesw	linesw[] =
 {
-	ttyopen, nulldev, ttread, ttwrite, nodev, ttyinput, ttstart, /* 0 */
-	0
+	{ttyopen, nulldev, ttread, ttwrite, nodev, ttyinput, ttstart}, /* 0 */
+	{0}
 };
 dev_t	rootdev	= makedev(9, 0);
 dev_t	swapdev	= makedev(9, 1);
