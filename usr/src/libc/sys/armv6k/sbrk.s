e3852ecdc7df7e0dd4c2e65fcab62312a321afc9
Michael van der Westhuizen <r1mikey@gmail.com>
2020-12-16 16:01:22 +0000

    libc: Use an 'nd' variable in sbrk (like the original)
    Signed-off-by: Michael van der Westhuizen <r1mikey@gmail.com>

--- usr/src/libc/sys/armv6k/sbrk.s
+++ usr/src/libc/sys/armv6k/sbrk.s
@@ -3,18 +3,19 @@
 .section .text
 
 .extern errno
+.extern _end
 
 
 @ void *sbrk(int incr)
 @ returns the start of the new area, or -1 on exit
 @ i.e. old = sbrk(incr);
 @
-@ We add the incr arg to the value of _end, then call the kernel break syscall.
-@ On success return, we stash the old _end, store our new value to _end and return the old.
+@ We add the incr arg to the value of __nd, then call the kernel break syscall.
+@ On success return, we stash the old __nd, store our new value to __nd and return the old.
 @
 @ When incr is 0, we do the same thing, just skip the kernel part.
 @
-@ brk seems worse - just push the value to the kernel, then set it in _end on success.
+@ brk seems worse - just push the value to the kernel, then set it in __nd on success.
 @
 .global sbrk
 .type sbrk,%function
@@ -23,12 +24,12 @@ sbrk:
     add     fp, sp, #0                                 @ adjust the frame pointer
     sub     sp, sp, #8                                 @ make a bit of space for locals
     str     r0, [fp, #-4]                              @ store the incr value as a local
-    ldr     r1, =_end                                  @ load up the address of _end (this is the value we want)
-    ldr     r1, [r1]                                   @ deref _end to get the current value
-    str     r1, [fp, #-8]                              @ store _end as a local
+    ldr     r1, =__nd                                  @ load up the address of __nd (this is the value we want)
+    ldr     r1, [r1]                                   @ deref __nd to get the current value
+    str     r1, [fp, #-8]                              @ store __nd as a local
     cmp     r0, #0                                     @ is incr 0?
     beq     2f                                         @ yep, incr is 0, skip to the out codepath
-    add     r0, r1, r0                                 @ add the new size to the value of _end to get what we pass to the kernel
+    add     r0, r1, r0                                 @ add the new size to the value of __nd to get what we pass to the kernel
     push    {r0}                                       @ ugh, stack based args
     mov     lr, sp                                     @ pointer to indirect args is kept in lr
     swi     #17                                        @ 17 is break, call the kernel
@@ -38,10 +39,10 @@ sbrk:
     mov     r0, #-1                                    @ syscalls return -1 for error
     b       1f                                         @ jump to the epilogue to return the error to the caller
 2:  ldr     r1, [fp, #-4]                              @ load up the original incr
-    ldr     r2, [fp, #-8]                              @ load up the original _end
+    ldr     r2, [fp, #-8]                              @ load up the original __nd
     add     r0, r2, r1                                 @ we'll save the new break end
-    ldr     r1, =_end                                  @ load up the address of _end
-    str     r0, [r1]                                   @ save the new value of _end
+    ldr     r1, =__nd                                  @ load up the address of __nd
+    str     r0, [r1]                                   @ save the new value of __nd
     mov     r0, r2                                     @ we'll return the original break end
 1:  sub     sp, r11, #0                                @ restore our stack pointer to point to our stack frame
     pop     {fp, pc}                                   @ restore caller frame and return
@@ -61,9 +62,13 @@ brk:
     str     r0, [r2]                                   @ stash the returned errno in the error case
     mov     r0, #-1                                    @ syscalls return -1 for error
     b       1f                                         @ jump to the epilogue to return the error to the caller
-2:  ldr     r1, =_end                                  @ load up the address of _end
-    str     r0, [r1]                                   @ store what we passed to the kernel to _end
+2:  ldr     r1, =__nd                                  @ load up the address of __nd
+    str     r0, [r1]                                   @ store what we passed to the kernel to __nd
     mov     r0, #0                                     @ our success return is 0
 1:  sub     sp, r11, #0                                @ restore our stack pointer to point to our stack frame
     pop     {fp, pc}                                   @ restore caller frame and return
 .size brk, . - brk
+
+
+.data
+__nd: .word _end
