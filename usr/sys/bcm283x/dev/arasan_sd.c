c4b3eab956f844771fa2303e45f3750bbb225d06
Michael van der Westhuizen <r1mikey@gmail.com>
2021-06-06 19:08:36 +0100

    Revert "Liberally sprinkle DMB on the Arasan access code, rather than on each register access"
    This reverts commit d020121b5716b0b1f4d0234d3096587f0da2869d.

--- usr/sys/bcm283x/dev/arasan_sd.c
+++ usr/sys/bcm283x/dev/arasan_sd.c
@@ -35,6 +35,9 @@ static u32 arasan_sd_readreg(struct arasan_sd_io_t *io, u32 a)
 static void arasan_sd_writereg(struct arasan_sd_io_t *io, u32 a, u32 v)
 {
   udelay(io->fast ? 2 : 20);
+  do_arm1176jzfs_dmb();
+  do_arm1176jzfs_dsb();
+  do_arm1176jzfs_isb();
   iowrite32(ARASAN_REGADDR(io, a), v);
 }
 #undef ARASAN_REGADDR
@@ -237,13 +240,11 @@ static void arasan_sd_io_fini(uptr_t handle)
   if (!(io = from_uptr(handle)))
     return;
 
-  DMB;
   arasan_sd_reset(io);
   arasan_sd_writereg(io, EMMC_IRPT_EN_REG_OFFSET, 0x0U);
   arasan_sd_writereg(io, EMMC_IRPT_MASK_REG_OFFSET, 0x0U);
   arasan_sd_writereg(io, EMMC_INTERRUPT_REG_OFFSET, 0xffffffffU);
   arasan_sd_io_put(io);
-  DMB;
 }
 
 static uptr_t arasan_sd_io_init(uptr_t iobase, u32 extclk)
@@ -256,8 +257,6 @@ static uptr_t arasan_sd_io_init(uptr_t iobase, u32 extclk)
 
   io->extclk = extclk;
   io->fast = 0;
-
-  DMB;
 
   if (0 != (ret = arasan_sd_reset(io)))
     goto out;
@@ -275,8 +274,6 @@ static uptr_t arasan_sd_io_init(uptr_t iobase, u32 extclk)
   arasan_sd_writereg(io, EMMC_INTERRUPT_REG_OFFSET, 0xffffffffU);
 
 out:
-  DMB;
-
   if (ret) {
     arasan_sd_io_put(io);
     return 0;
@@ -383,12 +380,9 @@ static int arasan_sd_io_cmd(uptr_t handle, u32 cmd, u32 arg, u32 *resp)
   if (0 != (ret = arasan_sd_io_cmdtm_get(cmd, &c)))
     return ret;
 
-  DMB;
   if (0 != (ret = arasan_sd_await_mask_not_in_reg(io, EMMC_STATUS_REG_OFFSET,
-      (CMD_INHIBIT_MASK|DAT_INHIBIT_MASK), NULL, 1000000))) {
-    DMB;
+      (CMD_INHIBIT_MASK|DAT_INHIBIT_MASK), NULL, 1000000)))
     return ret;
-  }
 
   /*
    * A "go to the idle state" is sent on card insertion, so reset data width
@@ -398,10 +392,8 @@ static int arasan_sd_io_cmd(uptr_t handle, u32 cmd, u32 arg, u32 *resp)
     arasan_sd_writereg(io, EMMC_CONTROL0_REG_OFFSET,
       arasan_sd_readreg(io, EMMC_CONTROL0_REG_OFFSET) &
         ~(HCTL_DWIDTH_MASK|HCTL_HS_EN_MASK));
-    if (0 != (ret = arasan_sd_setclk(io, FREQ_SETUP))) {
-      DMB;
+    if (0 != (ret = arasan_sd_setclk(io, FREQ_SETUP)))
       return ret;
-    }
     io->fast = 0;
   }
 
@@ -423,19 +415,13 @@ static int arasan_sd_io_cmd(uptr_t handle, u32 cmd, u32 arg, u32 *resp)
     i & ~(DATA_DONE_MASK|WRITE_RDY_MASK|READ_RDY_MASK|CARD_MASK));
   arasan_sd_writereg(io, EMMC_INTERRUPT_REG_OFFSET, 0xffff0001); /* XXX: yuck */
 
-  if (ret) {
-    DMB;
+  if (ret)
     return ret;
-  }
 
   if ((i & (CMD_DONE_MASK|ERR_MASK)) != CMD_DONE_MASK) {
-    if (arasan_sd_readreg(io, EMMC_STATUS_REG_OFFSET) & CMD_INHIBIT_MASK) {
-      if (0 != (ret = arasan_sd_reset_circuit(io, SRST_CMD_MASK))) {
-        DMB;
+    if (arasan_sd_readreg(io, EMMC_STATUS_REG_OFFSET) & CMD_INHIBIT_MASK)
+      if (0 != (ret = arasan_sd_reset_circuit(io, SRST_CMD_MASK)))
         return ret;
-      }
-    }
-    DMB;
     return (i & CTO_ERR_MASK) ? -EBUSY : -EIO;
   }
 
@@ -473,14 +459,10 @@ static int arasan_sd_io_cmd(uptr_t handle, u32 cmd, u32 arg, u32 *resp)
       EMMC_INTERRUPT_REG_OFFSET, i & (DATA_DONE_MASK|ERR_MASK));
     arasan_sd_writereg(io, EMMC_IRPT_MASK_REG_OFFSET, c);
     arasan_sd_writereg(io, EMMC_IRPT_EN_REG_OFFSET, v);
-    if (ret) {
-      DMB;
+    if (ret)
       return ret;
-    }
-    if ((i & (DATA_DONE_MASK|ERR_MASK)) != DATA_DONE_MASK) {
-      DMB;
+    if ((i & (DATA_DONE_MASK|ERR_MASK)) != DATA_DONE_MASK)
       return (i & DTO_ERR_MASK) ? -EBUSY : -EIO;
-    }
   }
 
   if (cmd == SD_CMD_SELECT_CARD ||
@@ -489,10 +471,8 @@ static int arasan_sd_io_cmd(uptr_t handle, u32 cmd, u32 arg, u32 *resp)
       cmd == SD_ACMD_SET_BUS_WIDTH) {
     if (!SD_CARD_STATUS_IS_ERROR(resp[0])) {
       if (cmd == SD_CMD_SELECT_CARD) {
-        if (0 != (ret = arasan_sd_setclk(io, FREQ_NORMAL))) {
-          DMB;
+        if (0 != (ret = arasan_sd_setclk(io, FREQ_NORMAL)))
           return ret;
-        }
         io->fast = 1;
       } else if (cmd == SD_CMD_VOLTAGE_SWITCH) {
         /* disable the clock */
@@ -500,20 +480,16 @@ static int arasan_sd_io_cmd(uptr_t handle, u32 cmd, u32 arg, u32 *resp)
           arasan_sd_readreg(io, EMMC_CONTROL1_REG_OFFSET) & ~(CLK_EN_MASK));
 
         /* if any DAT line is set we have a problem */
-        if (arasan_sd_readreg(io, EMMC_STATUS_REG_OFFSET) & 0x00f00000) {
-          DMB;
+        if (arasan_sd_readreg(io, EMMC_STATUS_REG_OFFSET) & 0x00f00000)
           return -ENXIO;
-        }
 
         /* set the 1v8 flag and ensure that it sticks */
         arasan_sd_writereg(io, EMMC_CONTROL0_REG_OFFSET,
           arasan_sd_readreg(io, EMMC_CONTROL0_REG_OFFSET) | (ENABLE_1_8V_MASK));
         udelay(5);
         if (!(arasan_sd_readreg(io,
-            EMMC_CONTROL0_REG_OFFSET) & (ENABLE_1_8V_MASK))) {
-          DMB;
+            EMMC_CONTROL0_REG_OFFSET) & (ENABLE_1_8V_MASK)))
           return -ENXIO;
-        }
 
         /* enable the clock */
         arasan_sd_writereg(io, EMMC_CONTROL1_REG_OFFSET,
@@ -521,24 +497,17 @@ static int arasan_sd_io_cmd(uptr_t handle, u32 cmd, u32 arg, u32 *resp)
         udelay(1);
 
         if (0 != (ret = arasan_sd_await_mask_in_reg(io,
-            EMMC_CONTROL1_REG_OFFSET, CLK_STABLE_MASK, NULL, 1000))) {
-          DMB;
+            EMMC_CONTROL1_REG_OFFSET, CLK_STABLE_MASK, NULL, 1000)))
           return ret;
-        }
 
         /* if any DAT line is set we're good */
-        if (!(arasan_sd_readreg(io, EMMC_STATUS_REG_OFFSET) & 0x00f00000)) {
-          DMB;
+        if (!(arasan_sd_readreg(io, EMMC_STATUS_REG_OFFSET) & 0x00f00000))
           return -ENXIO;
-        }
       } else if (cmd == SD_CMD_SWITCH_FUNC) {
         /* TODO: this is rubbish - we can do better */
-        if ((arg & 0x8000000f) == 0x80000001) {
-          if (0 != (ret = arasan_sd_setclk(io, FREQ_HS))) {
-            DMB;
+        if ((arg & 0x8000000f) == 0x80000001)
+          if (0 != (ret = arasan_sd_setclk(io, FREQ_HS)))
             return ret;
-          }
-        }
       } else if (cmd == SD_ACMD_SET_BUS_WIDTH) {
         v = arasan_sd_readreg(io, EMMC_CONTROL0_REG_OFFSET);
         switch (arg) {
@@ -555,7 +524,6 @@ static int arasan_sd_io_cmd(uptr_t handle, u32 cmd, u32 arg, u32 *resp)
     }
   }
 
-  DMB;
   return 0;
 }
 
@@ -572,15 +540,11 @@ static int arasan_sd_io_iosetup(uptr_t handle, u16 s, u16 c)
   if (v & BLKSIZECNT_RESERVED_MASK)
     return -EINVAL;
 
-  DMB;
   if (0 != (ret = arasan_sd_await_mask_not_in_reg(io,
-      EMMC_STATUS_REG_OFFSET, DAT_INHIBIT_MASK, NULL, 500000))) {
-    DMB;
+      EMMC_STATUS_REG_OFFSET, DAT_INHIBIT_MASK, NULL, 500000)))
     return ret;
-  }
 
   arasan_sd_writereg(io, EMMC_BLKSIZECNT_REG_OFFSET, v);
-  DMB;
   return 0;
 }
 
@@ -596,32 +560,24 @@ static int arasan_sd_io_iostart(uptr_t handle, int w, int a, u32 ba, u8 *buf, u3
   if (!(io = from_uptr(handle)))
     return -ENODEV;
 
-  DMB;
-
   arasan_sd_writereg(io, EMMC_INTERRUPT_REG_OFFSET,
     arasan_sd_readreg(io, EMMC_INTERRUPT_REG_OFFSET));
 
   /* TODO: maybe reset data circuitry? */
   if (0 != (ret = arasan_sd_await_mask_not_in_reg(io,
-      EMMC_STATUS_REG_OFFSET, DAT_INHIBIT_MASK, NULL, 500000))) {
-    DMB;
+      EMMC_STATUS_REG_OFFSET, DAT_INHIBIT_MASK, NULL, 500000)))
     return ret;
-  }
 
   cmd = w ? SD_CMD_WRITE_BLOCK : SD_CMD_READ_SINGLE_BLOCK;
-  if (0 != (ret = arasan_sd_io_cmd(handle, cmd, ba, resp))) {
-    DMB;
+  if (0 != (ret = arasan_sd_io_cmd(handle, cmd, ba, resp)))
     return ret;
-  }
 
   v = DATA_DONE_MASK|ERR_MASK;
   if (w) {
     for (i = 0; i < len; i += 4) {
       if (0 != (ret = arasan_sd_await_mask_in_reg(io,
-          EMMC_STATUS_REG_OFFSET, WRITE_TRANSFER_MASK, NULL, 1000))) {
-        DMB;
+          EMMC_STATUS_REG_OFFSET, WRITE_TRANSFER_MASK, NULL, 1000)))
         return ret;
-      }
       arasan_sd_writereg(io, EMMC_DATA_REG_OFFSET,
         ((buf[i + 0] <<  0) & 0x000000ff) |
         ((buf[i + 1] <<  8) & 0x0000ff00) |
@@ -634,7 +590,6 @@ static int arasan_sd_io_iostart(uptr_t handle, int w, int a, u32 ba, u8 *buf, u3
   arasan_sd_writereg(io,
     a ? EMMC_IRPT_EN_REG_OFFSET : EMMC_IRPT_MASK_REG_OFFSET, v);
 
-  DMB;
   return 0;
 }
 
@@ -642,7 +597,6 @@ static int arasan_sd_io_iowait(uptr_t handle, int w, u32 max_us)
 {
   struct arasan_sd_io_t *io;
   u32 v;
-  int ret;
 
   if (!(io = from_uptr(handle)))
     return -ENODEV;
@@ -650,11 +604,8 @@ static int arasan_sd_io_iowait(uptr_t handle, int w, u32 max_us)
   v = DATA_DONE_MASK|ERR_MASK;
   v |= w ? WRITE_RDY_MASK : READ_RDY_MASK;
 
-  DMB;
-  ret = arasan_sd_await_mask_in_reg(io,
+  return arasan_sd_await_mask_in_reg(io,
     EMMC_INTERRUPT_REG_OFFSET, v, NULL, max_us);
-  DMB;
-  return ret;
 }
 
 static int arasan_sd_io_iofinish(uptr_t handle, int w, u8 *buf, u32 len)
@@ -668,13 +619,10 @@ static int arasan_sd_io_iofinish(uptr_t handle, int w, u8 *buf, u32 len)
   if (!(io = from_uptr(handle)))
     return -ENODEV;
 
-  DMB;
-
   i = arasan_sd_readreg(io, EMMC_INTERRUPT_REG_OFFSET);
 
   if (i & ERR_MASK) {
     arasan_sd_writereg(io, EMMC_INTERRUPT_REG_OFFSET, i);
-    DMB;
     return -EIO;
   }
 
@@ -682,17 +630,14 @@ static int arasan_sd_io_iofinish(uptr_t handle, int w, u8 *buf, u32 len)
     data = (u32 *)buf;
     for (j = 0; j < len/4; ++j) {
       if (0 != (ret = arasan_sd_await_mask_in_reg(io,
-          EMMC_STATUS_REG_OFFSET, READ_TRANSFER_MASK, NULL, 1000))) {
-        DMB;
+          EMMC_STATUS_REG_OFFSET, READ_TRANSFER_MASK, NULL, 1000)))
         return ret;
-      }
       data[j] = arasan_sd_readreg(io, EMMC_DATA_REG_OFFSET);
     }
   }
 
   arasan_sd_writereg(io, EMMC_IRPT_EN_REG_OFFSET, 0x0U);
   arasan_sd_writereg(io, EMMC_INTERRUPT_REG_OFFSET, i);
-  DMB;
   return 0;
 }
 
